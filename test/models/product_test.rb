c11afadc568ce1cbfd2445ff636eb8655332a7a3
liz tobin <lizmtobin@gmail.com>
2022-03-31 16:53:14 +0100

    check image ending format valid

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -14,7 +14,7 @@ class ProductTest < ActiveSupport::TestCase
     product = Product.new(title:       "My Book Title",
                           description: "yyy",
                           image_url:   "zzz.jpg")
-                          
+
     product.price = -1
     assert product.invalid?
     assert_equal ["must be greater than or equal to 0.01"],
@@ -29,28 +29,28 @@ class ProductTest < ActiveSupport::TestCase
     assert product.valid?
   end
 
-  # def new_product(image_url)
-  #   Product.new(title:       "My Book Title",
-  #               description: "yyy",
-  #               price:       1,
-  #               image_url:   image_url)
-  # end
+  def new_product(image_url)
+    Product.new(title:       "My Book Title",
+                description: "yyy",
+                price:       1,
+                image_url:   image_url)
+  end
 
-  # test "image url" do
-  #   ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
-  #            http://a.b.c/x/y/z/fred.gif }
-  #   bad = %w{ fred.doc fred.gif/more fred.gif.more }
+  test "image url" do
+    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
+             http://a.b.c/x/y/z/fred.gif }
+    bad = %w{ fred.doc fred.gif/more fred.gif.more }
 
-  #   ok.each do |image_url|
-  #     assert new_product(image_url).valid?,
-  #            "#{image_url} shouldn't be invalid"
-  #   end
+    ok.each do |image_url|
+      assert new_product(image_url).valid?,
+             "#{image_url} shouldn't be invalid"
+    end
 
-  #   bad.each do |image_url|
-  #     assert new_product(image_url).invalid?,
-  #            "#{image_url} shouldn't be valid"
-  #   end
-  # end
+    bad.each do |image_url|
+      assert new_product(image_url).invalid?,
+             "#{image_url} shouldn't be valid"
+    end
+  end
 
   # test "product is not valid without a unique title" do
   #   product = Product.new(title:       products(:ruby).title,
