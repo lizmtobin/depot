eeac22583d9a6ddd5129037c490ecb73bd19844a
liz tobin <lizmtobin@gmail.com>
2022-06-21 21:08:25 +0100

    relation test

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ApplicationRecord
+    has_many :line_items
+
+    before_destroy :ensure_not_referenced_by_any_line_item
+    #add hook method to be called in run time
+
     validates :title, :description, :image_url, presence: true
     validates :price, numericality: {greater_than_or_equal_to: 0.01}
     validates :title, uniqueness: true
@@ -6,4 +11,16 @@ class Product < ApplicationRecord
         with: %r{\.(gif|jpg|png)\Z}i,
         message: 'must be a URL for gif, jpg or png image'
     }
+
+    private #not available as an action in controllers
+
+    def ensure_not_referenced_by_any_line_item
+        unless line_items.empty?
+            errors.add(:base, 'Line Items present')
+            #access to errors object, which validate method also stores error messages.
+            #Here we associate the error with the base object
+            throw :abort 
+            #abort destroy if condition unmet
+        end
+    end
 end
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -48,6 +48,14 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to product_url(@product)
   end
 
+  test "should delete product in cart" do
+    assert_difference('Product.count', 0) do
+      delete product_url(products(:two))
+    end
+
+    assert_redirected_to products_url
+  end
+
   test "should destroy product" do
     assert_difference('Product.count', -1) do
       delete product_url(@product)
@@ -55,4 +63,5 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
     assert_redirected_to products_url
   end
+
 end
