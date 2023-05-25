4a24e44806d2a0e649b4e39a6be9ab6938b3e0d5
liz tobin <lizmtobin@gmail.com>
2023-05-25 18:45:17 +0100

    order validations

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,8 @@
 class Order < ApplicationRecord
+
+    validates :name, :address, :email, presence: true
+    validates :pay_type, inclusion: pay_types.keys
+
     enum pay_type: {
         "Check" => 0,
         "Credit card" => 1,
