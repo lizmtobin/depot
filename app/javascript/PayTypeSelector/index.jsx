58198515afe51c3e51dec0089ff5f2be24be7d07
liz tobin <lizmtobin@gmail.com>
2023-06-25 22:19:21 +0100

    console test

--- app/javascript/PayTypeSelector/index.jsx
+++ app/javascript/PayTypeSelector/index.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 
 class PayTypeSelector extends React.Component {
+  onPayTypeSelected = (event) => {
+    console.log(event.target.value);
+  }
   render() {
     return (
       <div className="field">
         <label htmlFor="order_pay_type">Pay type</label>
-        <select id="order_pay_type" name="order[pay_type]">
+        <select onChange={this.onPayTypeSelected} name="order[pay_type]">
           <option value="">Select a payment method</option>
           <option value="Check">Check</option>
           <option value="Credit card">Credit card</option>
