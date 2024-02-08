0f7b0d8d7b5e1824732b87f0039d105e2d9d62f8
liz tobin <lizmtobin@gmail.com>
2024-02-08 20:29:05 +0000

    update config for mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,6 +40,19 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # Mailer config
+  config.action_mailer.delivery_method = :smtp
+  
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587, # 465 for ssl
+    domain: 'gmail.com',
+    user_name: 'lizmtobin',
+    password: 'lizmtobin1',
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
